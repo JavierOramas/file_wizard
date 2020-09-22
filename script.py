97c02d5fa038a85bb04906ff88859c01831d20ec
Javier Oramas <javiale2000@gmail.com>
2020-09-22 12:45:43 -0400

    extract functionality added

--- script.py
+++ script.py
@@ -1,15 +1,36 @@
 import os
+import shutil
 from typer import Typer
 
 app = Typer()
+
 
 @app.command(name='purge', help='Remove all files within a directory with a given extension')
 def purge(directory:str, ext:str):
     
     for cp,dir,files in os.walk(directory):
         for file in files:
-            print(file)
             if file.endswith(ext):
+                print(f'Deleting {file}')
                 os.remove(file)
+
+
+@app.command(name='extract', help='extract all files within a directory with a given extension')
+def extract(directory:str, destination:str, ext:str ):
+    
+    os.makedirs(destination, exist_ok=True)
+    
+    for cp,dir,files in os.walk(directory):
+        
+        for file in files:
+            
+            if cp == destination:
+                continue
+            
+            if file.endswith(ext):
+                print(f'Moving {file}')
+                shutil.move(os.path.join(cp,file), os.path.join(destination,file))
+
+
 if __name__ == '__main__':
     app()
